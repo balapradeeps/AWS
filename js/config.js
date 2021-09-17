3664b8f8205341626bb78b16b54fb6bf80a8e407
Tino Moussu <45273788+Tiino1@users.noreply.github.com>
2021-09-17 02:22:13 +0200

    Add admin users client pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_ayi7uVMAN'
+        userPoolClientId: '9cripp7li7e61hcir9kqhld0t'
+        region: 'eu-west-3'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
