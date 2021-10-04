f54593fd09810d404bc72ab4493f9050f181bad7
Tino Moussu <45273788+Tiino1@users.noreply.github.com>
2021-10-04 03:46:46 +0200

    Fix hello lambda

--- serverless-framework/lambdas/handler.rb
+++ serverless-framework/lambdas/handler.rb
@@ -1,11 +1,12 @@
 require 'json'
 
 def hello(event:, context:)
-  {
-    statusCode: 200,
-    body: {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    }.to_json
-  }
+  begin
+    puts "Received Request: #{event}"
+    { statusCode: 200, body: JSON.generate("Go Serverless v1.0! Your function executed successfully! #{event['body']}") }
+  rescue StandardError => e  
+    puts e.message  
+    puts e.backtrace.inspect  
+    { statusCode: 400, body: JSON.generate("Bad request, please POST a request body!") }
+  end
 end
