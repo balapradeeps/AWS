7dce467ef8feab6e7d06b1e8b02acf2ea7ca3c4d
Tino Moussu <>
2021-10-08 17:48:11 +0200

    Add db env var to serverless.yml

--- serverless-framework/lambdas/examples/mongo.rb
+++ serverless-framework/lambdas/examples/mongo.rb
@@ -7,14 +7,14 @@ database_host = ENV['DB_HOST'] #ex: 'mongodb://sample-cluster.node.us-east-1.doc
 
 client_options = {
    database: ENV['DB_NAME'],
-   replica_set: ENV['REPLICA_SET_NAME'],
+   replica_set: ENV['DB_REPLICA_SET_NAME'],
    user: ENV['DB_USER_NAME'],
    password: ENV['DB_USER_PASSWORD'],
-   ssl_ca_cert = ENV['SSL_CA_CERT']
+   ssl_ca_cert = ENV['DB_SSL_CA_CERT']
    retry_writes: false,
 }
 
-def example_mongo_create(event:, context:)
+def mongo(event:, context:)
    ##Create a MongoDB client, open a connection to Amazon DocumentDB as a
    ##   replica set and specify the read preference as secondary preferred
    client = Mongo::Client.new(database_host, client_options)
