ca9d6554f63c54f636b3ff60b0d974723ab6cfcd
Tino Moussu <>
2021-10-08 17:26:44 +0200

    Specify deployment workflow + add env variables in lambda

--- infra/README.md
+++ infra/README.md
@@ -2,12 +2,20 @@
 - aws cli
 - terraform cli
 
-# Set up your credentials
+# Set up your configurations
 - run `aws configure`
+- customise `configs.tfvars`
 
-# variables to set or pass
-- github_pat
+# Usage
+```terraform plan -var-file="configs.tfvars"```
 
-# Notes
-The app build will be queued at AWS, but it can be long
-terraform plan -var-file="configs.tfvars"
+# Workflow [WIP]
+#### Step 1
+- create a VPC
+- install documentdb in the VPC
+#### Step 2
+- download ssl_ca_certificate (.pem) file
+- get db host url
+#### Step 3
+- deploy lambdas in the VPC, with the pem file
+- deploy front app
---
+++ serverless-framework/lambdas/examples/example_mongo_create.rb
@@ -0,0 +1,40 @@
+
+require 'mongo'
+require 'neatjson'
+require 'json'
+
+database_host = ENV['DB_HOST'] #ex: 'mongodb://sample-cluster.node.us-east-1.docdb.amazonaws.com:27017'
+
+client_options = {
+   database: ENV['DB_NAME'],
+   replica_set: ENV['REPLICA_SET_NAME'],
+   user: ENV['DB_USER_NAME'],
+   password: ENV['DB_USER_PASSWORD'],
+   ssl_ca_cert = ENV['SSL_CA_CERT']
+   retry_writes: false,
+}
+
+def example_mongo_create(event:, context:)
+   ##Create a MongoDB client, open a connection to Amazon DocumentDB as a
+   ##   replica set and specify the read preference as secondary preferred
+   client = Mongo::Client.new(database_host, client_options)
+   
+   ##Insert a single document
+   x = client[:test].insert_one({"hello":"Amazon DocumentDB"})
+   
+   ##Find the document that was previously written
+   result = client[:test].find()
+   
+   #Print the document
+   result.each do |document|
+      puts JSON.neat_generate(document)
+   end
+
+   #Close the connection
+   client.close
+rescue
+  # error handling
+  puts e.backtrace.inspect
+  puts e.message
+  { statusCode: 400, body: JSON[{"unexpected_error" => "#{e.message}"}]}
+end
