d495ca8891d2a443efd31c13d06a351ad2ec5216
Tino Moussu <>
2021-10-11 16:09:16 +0200

    Create vpc main subnet

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,6 +1,11 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vpc" {
   cidr_block = yamldecode(file("../env.yml"))["VPC_CIDR_BLOCK"]
   tags = {
-    Name = "${yamldecode(file("../env.yml"))["VPC_NAME"]}"
+    Name = yamldecode(file("../env.yml"))["VPC_NAME"]
   }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = yamldecode(file("../env.yml"))["VPC_MAIN_SUBNET"]
 }
