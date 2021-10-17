1093f6c7b6f7e8de7d0b7d796aebaff7441ead6e
Tino Moussu <>
2021-10-17 02:46:47 +0200

    Fix host var

--- serverless-framework/lambdas/examples/mongo.rb
+++ serverless-framework/lambdas/examples/mongo.rb
@@ -3,28 +3,25 @@ require 'mongo'
 require 'neatjson'
 require 'json'
 
-database_host = ENV['DB_HOST'] #ex: 'mongodb://sample-cluster.node.us-east-1.docdb.amazonaws.com:27017'
+full_database_host = "mongodb://#{ENV['DB_HOST']}:#{ENV['DB_PORT']}" #ex: 'mongodb://sample-cluster.node.us-east-1.docdb.amazonaws.com:27017'
 
 client_options = {
-   database: ENV['DB_NAME'],
-   replica_set: ENV['DB_REPLICA_SET_NAME'],
-   user: ENV['DB_USER_NAME'],
-   password: ENV['DB_USER_PASSWORD'],
-   ssl_ca_cert = ENV['DB_SSL_CA_CERT']
-   retry_writes: false,
+   # user: ENV['DB_USER_NAME'],
+   # password: ENV['DB_USER_PASSWORD'],
+   retry_writes: false
 }
 
 def mongo(event:, context:)
    ##Create a MongoDB client, open a connection to Amazon DocumentDB as a
    ##   replica set and specify the read preference as secondary preferred
-   client = Mongo::Client.new(database_host, client_options)
-   
+   client = Mongo::Client.new(full_database_host, client_options)
+
    ##Insert a single document
    x = client[:test].insert_one({"hello":"Amazon DocumentDB"})
-   
+
    ##Find the document that was previously written
    result = client[:test].find()
-   
+
    #Print the document
    result.each do |document|
       puts JSON.neat_generate(document)
